cfc875f5fa25a9059eef6cf6a4e3099fdc5d25dc
Honoré Blaise <honoreblaise64@gmail.com>
2025-04-22 17:16:24 +0200

    Rajout des pages infos et rajoutFlux

---
+++ SAE2/AjouterFluxViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AjouterFluxViewController.swift
+//  SAE2
+//
+//  Created by etudiant on 22/04/2025.
+//
+
+import UIKit
+
+class AjouterFluxViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tapSurRetour: UIButton!
+    @IBOutlet weak var typeFluxSC: UISegmentedControl!
+    @IBOutlet weak var titreFluxTF: UITextField!
+    @IBOutlet weak var dateFluxDP: UIDatePicker!
+    @IBOutlet weak var tapSurAnnuler: UIButton!
+    @IBOutlet weak var tapSurAjouter: UIButton!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
